b79ddf7b78d7f54cc97ad75af96ebf7258f1eae2
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2022-11-28 16:00:52 +0100

    we continued cooking

---
+++ index.js
@@ -0,0 +1,56 @@
+console.log("Spaghetti alla Carbonara Recipe");
+
+let spaghetti = 400;
+let guanciale = 250;
+let eggs = 108;
+let peporinoRomano = 50;
+let blackPepper = 4;
+
+console.log("these are our ingredients:");
+console.log("Spaghetti:", spaghetti);
+console.log("Guanciale:", guanciale);
+console.log("Eggs:", eggs);
+console.log("Peporino Romano:", peporinoRomano);
+console.log("Black pepper:", blackPepper);
+
+let slicedGuanciale = guanciale /4 ; 
+console.log("Now we slice the guanciale into 1cm slices:", slicedGuanciale);
+
+let longStrips;
+longStrips = slicedGuanciale * 4;
+
+console.log("Now we cut the slices into long strips:", longStrips);
+
+let cheesyEgg;
+cheesyEgg = eggs + peporinoRomano;
+
+console.log("We added the eggs and the peporino Romano together:", cheesyEgg);
+
+cheesyEgg = blackPepper /4;
+
+console.log("..small amount of black Pepper to the egg and cheese mix:", cheesyEgg);
+
+let crispGuanciale;
+crispGuanciale = longStrips -1;
+
+console.log("we cooked the long strips of Guanciale and removed the grease:", crispGuanciale);
+
+let pastaWater;
+pastaWater = spaghetti +1000;
+console.log("we cooked the pasta with a liter of water and a bit of salt:", pastaWater);
+
+let ladefulWater;
+ladefulWater = pastaWater - 200;
+
+console.log("now we have this pasta Water left after removing a ladeful:", pastaWater);
+
+let reservedHotWater;
+reservedHotWater = ladefulWater - 2;
+
+let creamySauce
+creamySauce = guanciale + reservedHotWater; 
+
+console.log("we've added two spoons of water to the Guanicale:", creamySauce);
+
+let mixedPasta;
+mixedPasta = creamySauce + 
